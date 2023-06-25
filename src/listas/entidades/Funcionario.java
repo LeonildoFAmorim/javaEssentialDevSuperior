61a29e1655a7fde1657fe2f6ec64615bb634e629
Leonildo Filgueira <leonildolf_12@hotmail.com>
2023-06-25 01:11:43 -0300

    resolvendo exercício lista

---
+++ src/listas/entidades/Funcionario.java
@@ -0,0 +1,47 @@
+package listas.entidades;
+
+public class Funcionario {
+	private int id;
+	private String nome;
+	private double salario;
+
+	public Funcionario(int id, String nome, double salario) {
+		this.nome = nome;
+		this.id = id;
+		this.salario = salario;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public void aumentarSalario(double porcentagem) {
+		salario += salario * porcentagem / 100;
+	}
+
+	@Override
+	public String toString() {
+		return "ID:" + id + ", nome:" + nome + ", salario:" + String.format("%.2f", salario);
+	}
+
+}
---
+++ src/listas/programa/Programa.java
@@ -0,0 +1,71 @@
+package listas.programa;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import listas.entidades.Funcionario;
+
+public class Programa {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("Quantos funcionários serão registrados? ");
+		int totalFuncionarios = scanner.nextInt();
+
+		List<Funcionario> funcionarios = new ArrayList<>();
+
+		for (int i = 0; i < totalFuncionarios; i++) {
+			System.out.println("Funcionário #" + (i + 1));
+			System.out.print("ID: ");
+			int id = scanner.nextInt();
+			while (idRepetido(funcionarios, id)) {
+				System.out.print("ID repetido, digite outro ID: ");
+				id = scanner.nextInt();
+			}
+			System.out.print("Nome: ");
+			String nome = scanner.next();
+			System.out.print("Salário: ");
+			double salario = scanner.nextDouble();
+			funcionarios.add(new Funcionario(id, nome, salario));
+		}
+		System.out.print("Digite o ID do funcionário que terá o salário aumentado em 10%: ");
+		int id = scanner.nextInt();
+		if (idConstaNaLista(funcionarios, id)) {
+			System.out.print("Qual a porcentagem de aumento? ");
+			double porcentagemAumento = scanner.nextDouble();
+			for (Funcionario funcionario : funcionarios) {
+				if (funcionario.getId() == id) {
+					funcionario.aumentarSalario(porcentagemAumento);
+				}
+			}
+		} else {
+			System.out.println("ID não encontrado na lista.");
+		}
+
+		System.out.println();
+		for (Funcionario funcionario : funcionarios) {
+			System.out.println(funcionario);
+		}
+	}
+
+	public static boolean idRepetido(List<Funcionario> funcionarios, int id) {
+		boolean buscaId = false;
+		for (Funcionario funcionario : funcionarios) {
+			if (funcionario.getId() == id) {
+				buscaId = true;
+			}
+		}
+		return buscaId;
+	}
+
+	public static boolean idConstaNaLista(List<Funcionario> funcionarios, int id) {
+		boolean buscaId = false;
+		for (Funcionario funcionario : funcionarios) {
+			if (funcionario.getId() == id) {
+				buscaId = true;
+			}
+		}
+		return buscaId;
+	}
+}
